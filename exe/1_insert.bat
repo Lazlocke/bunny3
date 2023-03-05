175b5486d0349de95a5c9f5fe956739cfb155d21
esperknight <esperknight@yahoo.com>
2023-03-05 18:09:08 -0500

    Fixed weird bug where exe says it was in use when inserting...

--- exe/1_insert.bat
+++ exe/1_insert.bat
@@ -1,6 +1,7 @@
-del error.txt
-del /q ins\*
-copy orig\* ins
+del /q ins\Bunny3.exe
+copy orig\Bunny3.exe ins\Bunny3.exe
+
+pause 
 
 tools\bunny_black_3_exe_text_insertion.exe trans\000AF110.txt ins\Bunny3.exe
 
